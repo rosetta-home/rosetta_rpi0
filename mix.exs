ea1f2fecc88bd8757091820ed71cd30cedae6899
Christopher Coté <Christopher Cot>
2017-09-15 16:24:04 -0500

    adds doc dep

--- mix.exs
+++ mix.exs
@@ -25,6 +25,7 @@ defmodule RosettaRpi0.Mixfile do
       {:nerves, "~> 0.7", runtime: false },
       {:nerves_system_br, "~> 0.13.7", runtime: false },
       {:nerves_toolchain_armv6_rpi_linux_gnueabi, "~> 0.11.0", runtime: false}
+      {:ex_doc, ">= 0.0.0", only: :dev},
     ]
   end
 
